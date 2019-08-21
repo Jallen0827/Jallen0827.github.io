8fe4f222db92afcecdcff3d7d82007ed7004c770
Jallen <JustinAllen0827@gmail.com>
2019-08-21 11:38:00 -0400

    Portfolio as of 8-21

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const menuIcon = document.querySelector('.hamburgerMenu');
+const navItems = document.querySelector('.navItems');
+const navBar = document.querySelector('.navBar');
+
+
+menuIcon.addEventListener('click', ()=>{
+    navItems.classList.toggle('change');
+    navBar.classList.toggle('change');
+})
